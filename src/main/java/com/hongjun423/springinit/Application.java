e15eddcae49c75724f5359b4b78654123e074f39
hongjun423 <hongjc423@naver.com>
2019-08-27 23:04:19 +0900

    내장 톰캣 설정
    - 설정 실패
    - 내장 톰캣 9 와 관련된 부분 참고해서 수정 필요

--- src/main/java/com/hongjun423/springinit/Application.java
+++ src/main/java/com/hongjun423/springinit/Application.java
@@ -1,18 +1,52 @@
 package com.hongjun423.springinit;
 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.apache.catalina.Context;
+//import org.apache.catalina.LifecycleException;
+//import org.apache.catalina.startup.Tomcat;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
 
 
-@SpringBootApplication
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(args);
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
 
-//        SpringApplication.run(Application.class, args);
+        Context context = tomcat.addContext("/", "/");
+
+        HttpServlet servlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("하이");
+            }
+        };
+
+        String servletName = "helloServlet";
+        tomcat.addServlet("/", servlet);
+        context.addServletMappingDecoded("/hello", servletName);
+
+        tomcat.getConnector();
+        tomcat.start();
+        tomcat.getServer().await();
+
+
     }
 }
