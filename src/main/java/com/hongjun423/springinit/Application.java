e1479707f27c2a017dfab596290741677e40064f
hongjun423 <hongjc423@naver.com>
2019-09-09 22:03:25 +0900

    내장 웹 서버 응용 - https/http 설정
    - https를 설정한뒤 connector를 추가하여 htttp도 사용한다
    - https:8443
    - http: 8080

--- src/main/java/com/hongjun423/springinit/Application.java
+++ src/main/java/com/hongjun423/springinit/Application.java
@@ -1,7 +1,11 @@
 package com.hongjun423.springinit;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +18,26 @@ public class Application {
     }
 
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(){
         return "hello";
+    }
+
+    /**
+     * 8080 port 로 접근하는 http 커넥터
+     *
+     * @return
+     */
+    @Bean
+    public ServletWebServerFactory serverFactory(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
     }
 
 }
