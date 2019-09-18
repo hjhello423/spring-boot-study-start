312e6bef932084177cbf09120896ecb017d18600
hongjun423 <hongjc423@naver.com>
2019-09-18 21:47:09 +0900

    스프링 banner 파일 만들기

--- src/main/java/com/hongjun423/springinit/Application.java
+++ src/main/java/com/hongjun423/springinit/Application.java
@@ -1,43 +1,21 @@
 package com.hongjun423.springinit;
 
-import org.apache.catalina.connector.Connector;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
-@RestController
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication app = new SpringApplication(Application.class);
+//        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+
     }
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
-    }
-
-    /**
-     * 8080 port 로 접근하는 http 커넥터
-     *
-     * @return
-     */
-    @Bean
-    public ServletWebServerFactory serverFactory(){
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(8080);
-        return connector;
-    }
 
 }
