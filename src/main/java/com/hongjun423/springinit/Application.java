43048d09fc0187e147abac905c7ccca1d15a6f36
hongjun423 <hongjc423@naver.com>
2019-09-18 21:48:11 +0900

    스프링 banner 파일 만들기
    - 스프링 설정에 서 코드상으로 배너 만들기

--- src/main/java/com/hongjun423/springinit/Application.java
+++ src/main/java/com/hongjun423/springinit/Application.java
@@ -12,6 +12,19 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
+        app.setBanner(new Banner() {
+            @Override
+            public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
+                out.println("========================");
+                out.println("========================");
+                out.println("========================");
+                out.println("========================");
+                out.println("set baner");
+                out.println("========================");
+                out.println("========================");
+                out.println("========================");
+            }
+        });
 //        app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
 
