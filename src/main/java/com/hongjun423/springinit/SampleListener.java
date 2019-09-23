e6d15273eb31a799e26ac6926489b77364a12a75
hongjun423 <hongjc423@naver.com>
2019-09-23 21:34:47 +0900

    스프링 argument 받기 - ApplicationRunner

--- src/main/java/com/hongjun423/springinit/SampleListener.java
+++ src/main/java/com/hongjun423/springinit/SampleListener.java
@@ -1,10 +1,18 @@
 package com.hongjun423.springinit;
 
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SampleListener {
+public class SampleListener implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Runner foo: " + args.containsOption("foo"));
+        System.out.println("Runner bar: " + args.containsOption("bar"));
+
+    }
 
     public SampleListener(ApplicationArguments arguments) {
         System.out.println("foo: " + arguments.containsOption("foo"));
