4aadca479a44639dc6819d7b9240c64b2bfcdbf7
hongjun423 <hongjc423@naver.com>
2019-09-23 21:13:30 +0900

    스프링 이벤트 등록 하기 - ApplicationStartingEvent

--- src/main/java/com/hongjun423/springinit/Application.java
+++ src/main/java/com/hongjun423/springinit/Application.java
@@ -1,30 +1,16 @@
 package com.hongjun423.springinit;
 
-import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.core.env.Environment;
-
-import java.io.PrintStream;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder()
-                .sources(Application.class)
-                .banner(new Banner() {
-                    @Override
-                    public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out) {
-                        out.println("***************");
-                        out.println("***************");
-                        out.println("banner ----");
-                        out.println("***************");
-                        out.println("***************");
-                    }
-                })
-                .run();
+        SpringApplication app = new SpringApplication(Application.class);
+        app.addListeners(new SampleListener());
 
+        app.run(args);
     }
 
 
