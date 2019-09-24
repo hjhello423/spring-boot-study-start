a4d266e045a94cdb069a73762ccc5df5f9b80158
hongjun423 <hongjc423@naver.com>
2019-09-24 22:29:10 +0900

    application.properties 파일이용하여 외부 설정 하기

--- src/main/java/com/hongjun423/springinit/SampleListener.java
+++ src/main/java/com/hongjun423/springinit/SampleListener.java
@@ -1,5 +1,6 @@
 package com.hongjun423.springinit;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -7,16 +8,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleListener implements ApplicationRunner {
 
+    @Value("${test.name}")
+    private String name;
+
+    @Value("${test.age}")
+    private int age;
+
+    @Value("${test.fullName}")
+    private String fullName;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("Runner foo: " + args.containsOption("foo"));
-        System.out.println("Runner bar: " + args.containsOption("bar"));
-
-    }
-
-    public SampleListener(ApplicationArguments arguments) {
-        System.out.println("foo: " + arguments.containsOption("foo"));
-        System.out.println("bar: " + arguments.containsOption("bar"));
+        System.out.println("============================");
+        System.out.println(name +", " + age);
+        System.out.println(fullName);
+        System.out.println("============================");
 
     }
 }
