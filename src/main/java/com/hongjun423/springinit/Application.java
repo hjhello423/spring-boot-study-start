9d333947992c11e79dfee955237b748018ebb4ff
hongjun423 <hongjc423@naver.com>
2019-09-25 22:14:38 +0900

    @EnableConfigurationProperties 삭제

--- src/main/java/com/hongjun423/springinit/Application.java
+++ src/main/java/com/hongjun423/springinit/Application.java
@@ -3,10 +3,8 @@ package com.hongjun423.springinit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(MyProperties.class)
 public class Application {
 
     public static void main(String[] args) {
