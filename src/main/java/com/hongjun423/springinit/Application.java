fbfdeb21084aa78f02ef4ae9e70db5ba608b2df4
hongjun423 <hongjc423@naver.com>
2019-09-25 21:34:18 +0900

    @ConfigurationProperties 사용 하여 외부 설정 값 사용하기

--- src/main/java/com/hongjun423/springinit/Application.java
+++ src/main/java/com/hongjun423/springinit/Application.java
@@ -3,8 +3,10 @@ package com.hongjun423.springinit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MyProperties.class)
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/hongjun423/springinit/SampleRunner.java
+++ src/main/java/com/hongjun423/springinit/SampleRunner.java
@@ -1,27 +1,22 @@
 package com.hongjun423.springinit;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SampleListener implements ApplicationRunner {
+public class SampleRunner implements ApplicationRunner {
 
-    @Value("${test.name}")
-    private String name;
+    @Autowired
+    MyProperties myProperties;
 
-    @Value("${test.age}")
-    private int age;
-
-    @Value("${test.fullName}")
-    private String fullName;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("============================");
-        System.out.println(name +", " + age);
-        System.out.println(fullName);
+        System.out.println(myProperties.getName() + ", " + myProperties.getAge());
+        System.out.println(myProperties.getFullName());
         System.out.println("============================");
 
     }
