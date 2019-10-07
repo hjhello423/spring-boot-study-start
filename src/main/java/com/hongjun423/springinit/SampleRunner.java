2d9fe888182a2991634380d9cf05975d8f92120b
hongjun423 <hongjc423@naver.com>
2019-10-07 21:02:05 +0900

    스프링 로깅
    - 로깅 color 지정하기
    - 로그 기록되는 파일 지정 하기

--- src/main/java/com/hongjun423/springinit/SampleRunner.java
+++ src/main/java/com/hongjun423/springinit/SampleRunner.java
@@ -1,5 +1,7 @@
 package com.hongjun423.springinit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRunner implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(SampleRunner.class);
 
     @Autowired
     private String hello;
@@ -16,11 +20,12 @@ public class SampleRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("============================");
-        System.out.println(hello);
-        System.out.println(myProperties.getName());
-        System.out.println(myProperties.getFullName());
-        System.out.println("============================");
+
+        logger.warn("============================");
+        logger.info(hello);
+        logger.debug(myProperties.getName());
+        logger.info(myProperties.getFullName());
+        logger.warn("============================");
 
     }
 }
