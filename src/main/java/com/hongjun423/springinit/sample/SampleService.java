746029957f6e46da7178cbc0bc28506d98cdd50e
hongjun423 <hongjc423@naver.com>
2019-10-09 16:23:03 +0900

    mock를 이용한 테스트

---
+++ src/main/java/com/hongjun423/springinit/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.hongjun423.springinit.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "hongjun";
+    }
+}
